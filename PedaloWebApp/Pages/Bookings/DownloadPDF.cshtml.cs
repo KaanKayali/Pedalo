74482678dd8a613873f3dc7a851ba932235c639e
KaanKayali <citrusshooter1980@gmail.com>
2023-04-19 15:59:03 +0200

    PDF + Image

--- PedaloWebApp/Pages/Bookings/DownloadPDF.cshtml.cs
+++ PedaloWebApp/Pages/Bookings/DownloadPDF.cshtml.cs
@@ -1,15 +1,15 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using PedaloWebApp.Core.Interfaces.Data;
-using QuestPDF.Fluent;
-using QuestPDF.Helpers;
-using QuestPDF.Infrastructure;
-using System;
-using System.Linq;
-
 namespace PedaloWebApp.Pages.Bookings
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.EntityFrameworkCore;
+    using PedaloWebApp.Core.Interfaces.Data;
+    using QuestPDF.Fluent;
+    using QuestPDF.Helpers;
+    using QuestPDF.Infrastructure;
+    using System;
+    using System.Drawing;
+    using System.Linq;
     public class DownloadPDFModel : PageModel
     {
         private readonly IDbContextFactory contextFactory;
@@ -18,6 +18,7 @@ namespace PedaloWebApp.Pages.Bookings
         {
             this.contextFactory = contextFactory;
         }
+        public string today_string = DateTime.Now.ToString("yyyy-MM-dd");
         public IActionResult OnGet([FromQuery] Guid bookingId)
         {
             using var context = this.contextFactory.CreateReadOnlyContext();
@@ -26,7 +27,11 @@ namespace PedaloWebApp.Pages.Bookings
                 .Where(m => m.BookingId == bookingId)
                 .Include(x => x.BookingPassengers).ThenInclude(x => x.Passenger)
                 .Include(x => x.Customer)
+                .Include(x => x.Pedalo)
                 .FirstOrDefault();
+
+            int ColorValue = Color.FromName($"{booking.Pedalo.Color}").ToArgb();
+            string ColorHex = string.Format("{0:x6}", ColorValue);
 
 
             // code in your main method
@@ -40,35 +45,37 @@ namespace PedaloWebApp.Pages.Bookings
                     page.DefaultTextStyle(x => x.FontSize(25));
 
                     page.Header()
-                        .Text("Booking")
-                        .FontSize(36);
+                        .Text("Booking " + today_string)
+                        .FontSize(30);
 
                     page.Content()
                         .PaddingVertical(1, Unit.Centimetre)
                         .Column(x =>
                         {
                             x.Item().BorderTop(1);
-                            x.Item().Text("Pedalo:").FontSize(20).FontFamily("Segoe UI");
                             x.Item().Text("Customer:").FontSize(16);
                             x.Item().Text("Name: " + booking.Customer.FirstName + " " + booking.Customer.LastName + "\n").FontSize(14);
-                            //
-                            //x.Item().Text("Pedalo:").FontSize(16);
-                            //x.Item().Text("Name: " + booking.Pedalo.Name + "\n").FontSize(14);
-                            //x.Item().Text("Color: " + booking.Pedalo.Color + "\n").FontSize(14);
-                            //x.Item().Text("Capacity: " + booking.Pedalo.Capacity + "\n").FontSize(14);
-                            //x.Item().Text("Hourly rate: " + booking.Pedalo.HourlyRate + "\n").FontSize(14);
-                            //
-                            //x.Item().Text("Dates:").FontSize(16);
-                            //x.Item().Text("Start date: " + booking.StartDate + "\n").FontSize(14);
-                            //x.Item().Text("End date: " + booking.EndDate + "\n").FontSize(14);
+                            
+                            x.Item().Text("Pedalo:").FontSize(16);
+                            x.Item().Text("Name: " + booking.Pedalo.Name).FontSize(14);
+                            x.Item().Text("Color: " + booking.Pedalo.Color).FontColor(ColorHex).FontSize(14);
+                            x.Item().Text("Capacity: " + booking.Pedalo.Capacity).FontSize(14);
+                            x.Item().Text("Hourly rate: " + booking.Pedalo.HourlyRate + "\n").FontSize(14);
+                            
+                            x.Item().Text("Dates:").FontSize(16);
+                            x.Item().Text("Start date: " + booking.StartDate).FontSize(14);
+                            x.Item().Text("End date: " + booking.EndDate + "\n").FontSize(14);
 
                             x.Item().Text("Passengers:").FontSize(16);
                             foreach (var item in booking.BookingPassengers)
                             {
-                                x.Item().Text(item.Passenger.Firstname + " " + item.Passenger.Lastname + "\n").FontSize(14);
+                                x.Item().Text("-" + item.Passenger.Firstname + " " + item.Passenger.Lastname).FontSize(14);
                             }
+                            x.Item().Text("\n").FontSize(14);
                             x.Item().BorderTop(1);
+                            x.Item().Image("images/OIP.jpg");
                         });
+
 
                 });
             })
